2c1cd114cd84ee2f28bab55fd5f31611df7f73bd
rockethelll <rockethell@hotmail.com>
2023-01-17 11:18:44 +0100

    limitation entre 1 et 25 ok

--- exo_17.rb
+++ exo_17.rb
@@ -1,9 +1,13 @@
 p "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? (entre 1 et 25 maxi)"
 number = gets.chomp.to_i
 
-for num in 1..number
-  (number - num).times {print '-'}
-  num.times {print "#"}
-  (num - 1).times {print "#"}
-  puts
+if number >= 1 and number <= 25 then
+  for num in 1..number
+    (number - num).times {print ' '}
+    num.times {print "#"}
+    (num - 1).times {print "#"}
+    puts
+  end
+else 
+  p "Tu n'as pas entré un nombre entre 1 et 25 ..."
 end
